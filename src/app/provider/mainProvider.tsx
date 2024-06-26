a9862987d4ec6322624646c794d00204fc46045a
Alex <150617937+ChuddOff@users.noreply.github.com>
2024-06-26 12:36:21 +0300

    alfa

---
+++ src/app/provider/mainProvider.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { NextUIProvider } from "@nextui-org/system"
+
+export default function NextUI({children}: { children: React.ReactNode }) {
+  return (
+    <NextUIProvider className=" min-w-screen flex min-h-screen">
+      <div className=" w-full">
+        {children}
+      </div>
+    </NextUIProvider>
+  )
+}
