c40a37dcf8e0c5954bdefaf0c3c15a4c9d28e3b5
nik0504030201 <nikita0504030201@gmail.com>
2024-06-27 00:29:56 +0500

    переменные

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        violet: `#5a58ff`,
+        orange: `#FF6359`,
+      }
     },
   },
   darkMode: "class",
