d603706c4b701ade591cfb6897c830b8dcd814f3
l1ghttt <Nikita0504030201@gmail.com>
2024-07-10 07:44:05 +0500

    code layout

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -56,7 +56,7 @@ const AppModal: React.FC<ModalProps> = ({
           <div
               className={`relative gap-[30px] p-[50px] rounded-[50px] bg-[#c6c6c6] flex flex-col items-center w-[550px]`}
           >
-            <svg onClick={() => } className={`absolute left-[480px] top-[30px]`} width="48" height="48" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <svg className={`absolute left-[480px] top-[30px]`} width="48" height="48" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" clip-rule="evenodd"
                     d="M4.11 2.697L2.698 4.11 6.586 8l-3.89 3.89 1.415 1.413L8 9.414l3.89 3.89 1.413-1.415L9.414 8l3.89-3.89-1.415-1.413L8 6.586l-3.89-3.89z"
                     fill="#000"></path>
